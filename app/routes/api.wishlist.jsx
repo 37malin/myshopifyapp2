66cf1991137a2d4fb56030b01bae1ad2cf8fde4e
37malin <lesson37malin@gmail.com>
2024-05-09 15:12:47 +1000

    connect to prisma

--- app/routes/api.wishlist.jsx
+++ app/routes/api.wishlist.jsx
@@ -1,4 +1,6 @@
 import { json } from "@remix-run/node";
+import db from "../db.server";
+import { cors } from 'remix-utils/cors';
 
 export async function loader() {
   
@@ -10,13 +12,49 @@ export async function loader() {
 
 export async function action({ request }) {
     const method = request.method;
+    let data = await request.formData();
+    data = Object.fromEntries(data);
+    const customerId = data.customerId;
+    const productId = data.productId;
+    const shop = data.shop;
+
+    if(!customerId || !productId || !shop){
+        return json({
+            message: "Missing data. Required data: customerId, productId, shop",
+            method: method
+        })
+    }
 
     switch (method) {
         case "POST":
-            return json({message: "Success!!!", method: "POST"});
+            const wishlist = await db.wishlist.create({
+                data:{
+                    customerId,
+                    productId,
+                    shop
+                }
+            });
+
+            const response = json({ message: "Product added to wishlist", method: "POST", wishlist: wishlist });
+            return cors( request, response);
+
         case "PATCH":
-            return json({message: "Success!", method: "PATCH"});
+            return json({ message: "Success", method: "PATCH"});
+
+        case "DELETE":
+            await db.wishlist.deleteMany({
+                where: {
+                  customerId: customerId,
+                  shop: shop,
+                  productId: productId,
+                },
+              });
+        
+              response = json({ message: "Product removed from your wishlist", method: _action, wishlisted: false });
+              return cors(request, response);
+        
         default:
-            return new Response("Method Not Allowed", { status: 405});
+            return new Response("Method Not Allowed", { status: 405 });
+
     }
   }
